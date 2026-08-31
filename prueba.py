d35f9e4ed5cf070e170ff409a35d32864de4579e
Uli <dagos.ulises@gmail.com>
2026-08-31 08:57:15 -0300

    refactor: improve code formatting and implement user-zone mapping function

--- prueba.py
+++ prueba.py
@@ -4,8 +4,8 @@ import json
 def limpiar_valor(value: str) -> str | int:
     value = value.strip()
     try:
-        if(value.isnumeric() and value[0]=="0"):
-            return value           
+        if value.isnumeric() and value[0] == "0":
+            return value
         value = int(value)
     except ValueError:
         pass
@@ -13,9 +13,13 @@ def limpiar_valor(value: str) -> str | int:
 
 
 def parsear_titulos(titulos: str) -> str:
-    
-    return titulos[titulos.find("(")+1:titulos.rfind(")")].replace(" ", "").replace("`", "").split(",")
 
+    return (
+        titulos[titulos.find("(") + 1 : titulos.rfind(")")]
+        .replace(" ", "")
+        .replace("`", "")
+        .split(",")
+    )
 
 
 def parsear_fila(fila: str) -> list:
@@ -59,17 +63,17 @@ def json_creation(to_dump: dict, file_name: str) -> str:
 ####
 
 
-def parsear_archivo(file_name: str,output_dict_key:str) -> dict:
-    with open(file_name, "r",encoding='utf-8') as file:
+def parsear_archivo(file_name: str, output_dict_key: str) -> dict:
+    with open(file_name, "r", encoding="utf-8") as file:
         output: dict = {}
-        titulos: list = []  
+        titulos: list = []
         titulos = parsear_titulos(file.readline())
         # print(titulos)
-        filas_procesadas:int = 0
+        filas_procesadas: int = 0
 
         for line in file:
-            #chequeo que no sea linea de insert
-            if(line[0]!="("):
+            # chequeo que no sea linea de insert
+            if line[0] != "(":
                 continue
 
             fila_lista: list = parsear_fila(line)
@@ -85,15 +89,54 @@ def parsear_archivo(file_name: str,output_dict_key:str) -> dict:
 
             filas_procesadas += 1
 
-        assert filas_procesadas == len(output), "Hay claves repetidas, se pisaron usuarios"
+        assert filas_procesadas == len(output), (
+            "Hay claves repetidas, se pisaron usuarios"
+        )
         return output
 
 
-zonas_parseado: dict = parsear_archivo("zonas.sql","id")
-# print(zonas_parseado)
-usuarios_parseado:dict =  parsear_archivo("usuarios.sql","email")
+def cruzar_usuarios_x_zonas(zonas: dict, usuarios: dict) -> dict:
+    """
+    { "Zona Norte": [{usuario}, {usuario}], "Zona Sur": [{usuario}], ... }
+    """
+    output: dict = {}
+    for key, value in zonas.items():
+        output[
+            value["nombre"]
+        ] = []  # hago esto pq nlo tengo por id en usuarios, ya q era una tabla sql
+
+    for key_usuario, dict_usuario in usuarios.items():
+        try:
+            output = output[zonas[dict_usuario["zona_id"]]["nombre"]].append(
+                dict_usuario["nombre"],
+                dict_usuario["apellido"],
+                dict_usuario["email"],
+                dict_usuario["apellido"],
+                dict_usuario["telefono"],
+                dict_usuario["direccion"],
+            )
+
+        except KeyError:
+            pass
+        # print(output[zonas[dict_usuario["zona_id"]]["nombre"]])
+
+        # .append([value["nombre"]])
+        """
+        'zule@gmail.com': 
+            {'falta': '2015-11-23 14:22:51', 'fultimologin': '2020-03-17 21:35:03', 'fmodificacion': '2016-04-25 12:50:55',
+            'fultimopedido': 'NULL', 'nombre': 'Zuleica', 'apellido': 'Requelme', 'email': 'zule@gmail.com', 'telefono': 44, 'movil': 15,
+            'password': '0', 'domicilio': 'Av', 'altura': 0, 'piso': 3, 'departamento': 'A', 'zona_id': 14,
+            'sub_seccion_id': '0', 'observaciones': '', 'especial': 1, 'habilitado': 1, 'descuento': '0', 'tipo_usuario': 1, 'email_fb': 'NULL', 'token': 'NULL', 'no_newsletter': '0', 'vendedor': '0'}
+        """
+
+    print(output)
+
+
+zonas_parseado: dict = parsear_archivo("zonas.sql", "id")
+print(zonas_parseado)
+usuarios_parseado: dict = parsear_archivo("usuarios.sql", "email")
 # print(usuarios_parseado)
 
-usuarios_x_zonas
+usuarios_x_zonas: dict = cruzar_usuarios_x_zonas(zonas_parseado, usuarios_parseado)
 
 # created_file: str = json_creation(output, file_name)
